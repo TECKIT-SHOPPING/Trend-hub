873b8d05d742020075cab9d19ff5c06fc2e3bc07
Anwooseong <aws5624@naver.com>
2024-01-22 09:38:38 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/trendhub/trendhub/TrendHubApplication.java
@@ -0,0 +1,13 @@
+package com.trendhub.trendhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trendhub/trendhub/TrendHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trendhub.trendhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
