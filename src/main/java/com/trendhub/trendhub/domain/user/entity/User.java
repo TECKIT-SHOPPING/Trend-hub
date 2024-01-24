ad5cf5c80659d59a0d747ed0840bdc3cde940503
BuKwon Kim <dnu05043@naver.com>
2024-01-24 16:58:10 +0900

    로그인 기능 구현

--- src/main/java/com/trendhub/trendhub/domain/user/entity/User.java
+++ src/main/java/com/trendhub/trendhub/domain/user/entity/User.java
@@ -2,17 +2,23 @@ package com.trendhub.trendhub.domain.user.entity;
 
 import com.trendhub.trendhub.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
 public class User extends BaseTimeEntity {
 
     @Id
@@ -27,6 +33,21 @@ public class User extends BaseTimeEntity {
     @Temporal(TemporalType.DATE)
     private Date birth;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new SimpleGrantedAuthority("ROLE_MEMBER"));
+
+        /*if (List.of("system", "admin").contains(username)) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        }*/
+
+        return authorities;
+    }
+
     private String profile;
     private String provider; //app, kakao
     private LocalDateTime agreeInfo;
@@ -38,4 +59,5 @@ public class User extends BaseTimeEntity {
     private int level;
     private int point;
     private String status;
+
 }
