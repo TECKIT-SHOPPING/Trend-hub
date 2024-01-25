fbe895e9d09f8e54643386fd2d954790a0eb27f0
Anwooseong <aws5624@naver.com>
2024-01-26 02:08:05 +0900

    feat: 회원가입 화면 제작 & 회원가입 기능(이메일인증 등)

---
+++ src/main/java/com/trendhub/trendhub/domain/email/controller/EmailController.java
@@ -0,0 +1,36 @@
+package com.trendhub.trendhub.domain.email.controller;
+
+import com.trendhub.trendhub.domain.email.dto.SignUpEmailReq;
+import com.trendhub.trendhub.domain.email.dto.VerifyEmailReq;
+import com.trendhub.trendhub.domain.email.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/email")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @PostMapping("/join")
+    @ResponseBody
+    public boolean sendAuthEmail(@RequestBody SignUpEmailReq signUpEmailReq) throws Exception {
+        emailService.sendEmailAuth(signUpEmailReq.getEmail()+"@"+signUpEmailReq.getEmailDomain());
+        return true;
+    }
+
+    @PostMapping("/join/resend")
+    @ResponseBody
+    public boolean resendAuthEmail(@RequestBody SignUpEmailReq signUpEmailReq) throws Exception {
+        emailService.sendEmailAuth(signUpEmailReq.getEmail()+"@"+signUpEmailReq.getEmailDomain());
+        return true;
+    }
+
+    @GetMapping("/confirm")
+    @ResponseBody
+    public boolean confirmAuthEmail(@RequestParam("q") String q) {
+        return emailService.verifyAuthCode(q);
+    }
+}
