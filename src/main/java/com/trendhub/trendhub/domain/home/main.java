3e1d5a0fe9b6ba41396f978612aff2aceb68eb28
jinyoung121636 <damin0518@hanmail.net>
2024-01-29 13:59:20 +0900

    feat: 메인홈페이지 제작

--- src/main/java/com/trendhub/trendhub/domain/home/main.java
+++ src/main/java/com/trendhub/trendhub/domain/home/main.java
@@ -1,27 +1,26 @@
 package com.trendhub.trendhub.domain.home;
 
-import com.trendhub.trendhub.domain.home.dto.MainDto;
-import com.trendhub.trendhub.domain.home.service.MainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 @RequiredArgsConstructor
 public class main {
 
-    private final MainService mainService;
-
-    @GetMapping("/")
-        public String main(Model model) {
-
-            MainDto mainDto = mainService.getMainPage();
-            model.addAttribute("mainDto", mainDto);
-            return "main";
-        }
-
-        @GetMapping("/coordi")
-        public String community(){
-            return "coordi";
-        }
+    @GetMapping("/home")
+    public String main(){
+        return "main";
     }
+
+    @GetMapping("/community")
+    public String community(){
+        return "community";
+    }
+
+    @GetMapping("/membership")
+    public String membership(){
+        return "membership";
+    }
+}
