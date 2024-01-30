99eb6d295277149e8783b56fa6500c3a2dd6f555
Anwooseong <84388081+Anwooseong@users.noreply.github.com>
2024-01-30 17:27:24 +0900

    Feature/register user (#14)
    * feat: 회원가입 화면 제작 & 회원가입 기능(이메일인증 등)
    
    * fix: 회원가입 로직 수정(google.com -> gmail.com & 중복 아이디 체크 & email에 도메인 추가)
    
    * fix: 코드 충돌 해결

--- src/main/java/com/trendhub/trendhub/domain/user/controller/UserController.java
+++ src/main/java/com/trendhub/trendhub/domain/user/controller/UserController.java
@@ -19,7 +19,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -32,7 +31,6 @@ import java.security.Principal;
 
 @Slf4j
 @Controller
-@Transactional
 @RequiredArgsConstructor
 @RequestMapping("/members")
 public class UserController {
