d7dd2f676907af13acabb8a1b00d9cefa2f0a4ca
jinyoung121636 <damin0518@hanmail.net>
2024-02-01 23:00:18 +0900

    feat:코드수정(로그인 안됨)

--- src/main/java/com/trendhub/trendhub/global/config/security/SecurityConfig.java
+++ src/main/java/com/trendhub/trendhub/global/config/security/SecurityConfig.java
@@ -28,6 +28,9 @@ public class SecurityConfig {
                                         .failureUrl("/members/login/error")
 
                 )
+                /*.csrf(
+                        httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable()
+                )*/
                 .headers(
                         headers ->
                                 headers.frameOptions(
