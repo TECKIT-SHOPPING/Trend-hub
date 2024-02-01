dfd259102829f1c9c3536f1447ada7a37c40e78e
jinyoung121636 <damin0518@hanmail.net>
2024-02-01 19:36:46 +0900

    fix:충돌 수정 (19:36)

--- src/main/java/com/trendhub/trendhub/domain/home/main.java
+++ src/main/java/com/trendhub/trendhub/domain/home/main.java
@@ -4,30 +4,24 @@ import com.trendhub.trendhub.domain.home.dto.MainDto;
 import com.trendhub.trendhub.domain.home.service.MainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 @RequiredArgsConstructor
 public class main {
 
-<<<<<<< HEAD
     private final MainService mainService;
 
     @GetMapping("/")
-    public String main(Model model) {
+        public String main(Model model) {
 
-        MainDto mainDto = mainService.getMainPage();
-        model.addAttribute("mainDto", mainDto);
-=======
-    @GetMapping("/")
-    public String main(){
->>>>>>> 6aeb89e (feat/마이페이지 화면구현 충돌해결)
-        return "main";
+            MainDto mainDto = mainService.getMainPage();
+            model.addAttribute("mainDto", mainDto);
+            return "main";
+        }
+
+        @GetMapping("/coordi")
+        public String community(){
+            return "coordi";
+        }
     }
-
-    @GetMapping("/coordi")
-    public String community(){
-        return "coordi";
-    }
-
-}
