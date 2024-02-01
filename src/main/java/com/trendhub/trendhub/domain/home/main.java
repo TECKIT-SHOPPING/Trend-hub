aa93de74b2a2783324c91f39139fa90c51bce985
Anwooseong <aws5624@naver.com>
2024-02-01 17:45:41 +0900

    feat: 메인 페이지 기능 구현 중

--- src/main/java/com/trendhub/trendhub/domain/home/main.java
+++ src/main/java/com/trendhub/trendhub/domain/home/main.java
@@ -1,5 +1,7 @@
 package com.trendhub.trendhub.domain.home;
 
+import com.trendhub.trendhub.domain.home.dto.MainDto;
+import com.trendhub.trendhub.domain.home.service.MainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,8 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class main {
 
+    private final MainService mainService;
+
     @GetMapping("/")
-    public String main(){
+    public String main(Model model) {
+
+        MainDto mainDto = mainService.getMainPage();
+        model.addAttribute("mainDto", mainDto);
         return "main";
     }
 
