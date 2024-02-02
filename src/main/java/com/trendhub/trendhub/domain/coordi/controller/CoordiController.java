f5fcfc0136270f4b8583fbdb9d9aeeaa11e92c95
jinyoung121636 <damin0518@hanmail.net>
2024-02-02 13:07:47 +0900

    fix/myPage_1.html 수정

--- src/main/java/com/trendhub/trendhub/domain/coordi/controller/CoordiController.java
+++ src/main/java/com/trendhub/trendhub/domain/coordi/controller/CoordiController.java
@@ -1,11 +1,12 @@
 package com.trendhub.trendhub.domain.coordi.controller;
 
-import com.trendhub.trendhub.domain.coordi.dto.CoordiLikeDto;
 import com.trendhub.trendhub.domain.coordi.service.CoordiService;
-import com.trendhub.trendhub.domain.product.dto.ProductLikeDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
@@ -37,6 +38,4 @@ public class CoordiController {
     public String coordi(){
         return "coordi";
     }
-
-
 }
