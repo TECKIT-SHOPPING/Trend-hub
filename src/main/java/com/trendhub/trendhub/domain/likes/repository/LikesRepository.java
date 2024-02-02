4fd4e1d5e115c5dffe6f9c46ffe4bf0997384aa6
Anwooseong <aws5624@naver.com>
2024-02-02 17:11:33 +0900

    feat: 좋아요기능

--- src/main/java/com/trendhub/trendhub/domain/likes/repository/LikesRepository.java
+++ src/main/java/com/trendhub/trendhub/domain/likes/repository/LikesRepository.java
@@ -1,6 +1,5 @@
 package com.trendhub.trendhub.domain.likes.repository;
 
-import com.trendhub.trendhub.domain.coordi.entity.Coordi;
 import com.trendhub.trendhub.domain.likes.entity.Likes;
 import com.trendhub.trendhub.domain.product.entity.Product;
 import com.trendhub.trendhub.domain.user.entity.User;
@@ -11,6 +10,4 @@ import java.util.Optional;
 public interface LikesRepository extends JpaRepository<Likes, Long> {
 
     Optional<Likes> findByProductAndUser(Product product, User user);
-
-    Optional<Likes> findByCoordiAndUser(Coordi coordi, User user);
 }
