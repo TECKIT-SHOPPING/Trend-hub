cc62bf6833f3b15a456abac560edc1bf23de9c3d
BuKwon Kim <dnu05043@naver.com>
2024-02-11 00:20:51 +0900

    fix : 2/11 수정

--- src/main/java/com/trendhub/trendhub/domain/product/controller/ProductController.java
+++ src/main/java/com/trendhub/trendhub/domain/product/controller/ProductController.java
@@ -25,10 +25,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @RequestMapping("/products")
