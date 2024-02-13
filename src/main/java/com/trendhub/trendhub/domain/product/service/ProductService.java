bd16dd1a1556cb461584880096a6e5aca8ffd762
jinyoung121636 <damin0518@hanmail.net>
2024-02-13 13:56:35 +0900

    fix:코드 리팩토링

--- src/main/java/com/trendhub/trendhub/domain/product/service/ProductService.java
+++ src/main/java/com/trendhub/trendhub/domain/product/service/ProductService.java
@@ -6,6 +6,7 @@ import com.trendhub.trendhub.domain.product.dto.ProductDto;
 import com.trendhub.trendhub.domain.product.dto.ProductLikeDto;
 import com.trendhub.trendhub.domain.product.dto.QnaDto;
 import com.trendhub.trendhub.domain.product.entity.Product;
+import com.trendhub.trendhub.domain.product.entity.Season;
 import com.trendhub.trendhub.domain.product.entity.QnA;
 import com.trendhub.trendhub.domain.product.repository.ProductRepository;
 import com.trendhub.trendhub.domain.product.repository.QnaRepository;
